67a720a8c1a838c230f9fd7a7f7ef6802c5094d9
agayevmahammad <Mahammad.Agayev@kapitalbank.az>
2025-10-27 19:45:44 +0400

    Web Implementation

--- src/main/java/shared/config/framework/CucumberSpringConfiguration.java
+++
@@ -1,9 +0,0 @@
-package shared.config.framework;
-
-import io.cucumber.spring.CucumberContextConfiguration;
-import org.springframework.test.context.ContextConfiguration;
-
-@CucumberContextConfiguration
-@ContextConfiguration(classes = SpringConfig.class)
-public class CucumberSpringConfiguration {
-}
